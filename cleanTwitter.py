c0cb5ce26d5322329214be21bc14a635417bbe18
Justin Barnowski <jbarno54@gmail.com>
2022-04-05 20:01:20 -0500

    chore(black): just format the wasteland

--- cleanTwitter.py
+++ cleanTwitter.py
@@ -4,7 +4,8 @@
 import os
 import sys
 import argparse
-#import datetime 
+
+# import datetime
 from datetime import datetime, timedelta, time
 import configparser
 import time
@@ -13,7 +14,7 @@ import tweepy
 import pprint
 
 
-class TwitterClean():
+class TwitterClean:
     def __init__(self, args=None):
         self.script_dir = os.path.dirname(os.path.realpath(sys.argv[0]))
         self.config_path = os.path.join(self.script_dir, "settings.ini")
@@ -30,35 +31,43 @@ class TwitterClean():
             print("Please specify a valid config file.")
         else:
             try:
-                ck = config.get('Authentication', 'ConsumerKey')
-                cs = config.get('Authentication', 'ConsumerSecret')
-                at = config.get('Authentication', 'AccessToken')
-                ats = config.get('Authentication', 'AccessTokenSecret')
+                ck = config.get("Authentication", "ConsumerKey")
+                cs = config.get("Authentication", "ConsumerSecret")
+                at = config.get("Authentication", "AccessToken")
+                ats = config.get("Authentication", "AccessTokenSecret")
             except (configparser.NoSectionError,):
-                print("Please check the [Authentication] information in your configuration file.")
+                print(
+                    "Please check the [Authentication] information in your configuration file."
+                )
                 self.api = None
             else:
                 if all([ck, cs, at, ats]):
                     self.authenticate(ck, cs, at, ats)
                 else:
                     self.api = None
-                    print("Please check the options set under [Authentication] in your configuration file.")
+                    print(
+                        "Please check the options set under [Authentication] in your configuration file."
+                    )
 
-    def authenticate(self, consumer_key, consumer_secret, access_token, access_token_secret):
+    def authenticate(
+        self, consumer_key, consumer_secret, access_token, access_token_secret
+    ):
         auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
         auth.set_access_token(access_token, access_token_secret)
-        self.api = tweepy.API(auth, wait_on_rate_limit_notify=True, wait_on_rate_limit=True)
+        self.api = tweepy.API(
+            auth, wait_on_rate_limit_notify=True, wait_on_rate_limit=True
+        )
         try:
             self.me = self.api.me()
-            #self.me = None  # only used to test access
-            self.followers=self.api.followers_ids(id=self.me.id)
-            self.friends=self.api.friends_ids(id=self.me.id)
+            # self.me = None  # only used to test access
+            self.followers = self.api.followers_ids(id=self.me.id)
+            self.friends = self.api.friends_ids(id=self.me.id)
         except tweepy.error.TweepError as e:
             print("Please check the authentication information:\n{}".format(e))
             self.api = None
             self.me = None
 
-    def limit_handled(self,cursor):
+    def limit_handled(self, cursor):
         while True:
             try:
                 yield cursor.next()
@@ -67,81 +76,127 @@ class TwitterClean():
             except StopIteration:
                 return
 
-    def block_user(self,follower_id,screen_name=None):
-        """ Block user itself """
+    def block_user(self, follower_id, screen_name=None):
+        """Block user itself"""
         try:
             if follower_id not in self.followers:
                 if follower_id not in self.friends:
-                    print( "> user: %s is  not a friend"%(screen_name) )
+                    print("> user: %s is  not a friend" % (screen_name))
                     self.api.create_block(follower_id)
-                    print( "> user: %s is blocked"%(screen_name) )
+                    print("> user: %s is blocked" % (screen_name))
                     time.sleep(1)
                 else:
-                    print( "> user: %s is a friend" %(screen_name) )
+                    print("> user: %s is a friend" % (screen_name))
             else:
-                print( "> user: %s is a friend" %(screen_name) )
+                print("> user: %s is a friend" % (screen_name))
         except tweepy.error.TweepError:
-            print( "FAILED: Couldnt block user: %s UserID: %s" %(screen_name,follower_id) )
+            print(
+                "FAILED: Couldnt block user: %s UserID: %s" % (screen_name, follower_id)
+            )
 
-
-    def block_followers(self,target_user):
-        for follower_id in self.limit_handled(tweepy.Cursor(self.api.followers_ids,id=target_user).items()):
+    def block_followers(self, target_user):
+        for follower_id in self.limit_handled(
+            tweepy.Cursor(self.api.followers_ids, id=target_user).items()
+        ):
             try:
-                screen_name=self.api.get_user(id=follower_id).screen_name
-                print( "Blocking user: %s UserID: %s" %(screen_name,follower_id) )
-                self.block_user( follower_id,screen_name)
+                screen_name = self.api.get_user(id=follower_id).screen_name
+                print("Blocking user: %s UserID: %s" % (screen_name, follower_id))
+                self.block_user(follower_id, screen_name)
             except tweepy.error.TweepError:
                 print("ERROR:  couldn't get screen name or follower_id")
 
-    def unretweet(self,target_id):
-        """ If the wtweet is a retweet will unretweet it """
+    def unretweet(self, target_id):
+        """If the wtweet is a retweet will unretweet it"""
         status = self.api.get_status(target_id, tweet_mode="extended")
         if hasattr(status, "retweeted_status"):  # Check if Retweet
-            print( "%s Has attribute: retweeted_status" % (target_id) )
+            print("%s Has attribute: retweeted_status" % (target_id))
             self.api.unretweet(target_id)
             self.api.destroy_status(target_id)
         else:
-            print( "%s does NOT have attribute: rewteeted_status" % (target_id)  )
+            print("%s does NOT have attribute: rewteeted_status" % (target_id))
 
-    def unlike(self,target_id):
-        """ unlike tweet """
+    def unlike(self, target_id):
+        """unlike tweet"""
         try:
             self.api.destroy_favorite(target_id)
         except tweepy.error.TweepError as e:
-            print( "%s could not be unliked,trying like/unlike" % (target_id)  )
+            print("%s could not be unliked,trying like/unlike" % (target_id))
             try:
                 self.api.create_favorite(target_id)
                 self.api.destroy_favorite(target_id)
-                print( ">> %s likely unliked" % (target_id)  )
+                print(">> %s likely unliked" % (target_id))
             except:
-                print( ">> %s could not be unliked" % (target_id)  )
+                print(">> %s could not be unliked" % (target_id))
 
     def unlike_old_tweets(self, max_age=30):
-        for page in self.limit_handled( tweepy.Cursor(self.api.favorites).pages() ):
+        for page in self.limit_handled(tweepy.Cursor(self.api.favorites).pages()):
             for favorite in page:
-              try:
-                  if favorite.created_at < datetime.now() - timedelta(days=max_age) :
-                      print( ">> %s unliking old tweet from %s" % (favorite.id,favorite.created_at) )
-                      self.unlike( favorite.id )
-                  else:
-                      print( ">> %s keeping like on old tweet from %s" % (favorite.id,favorite.created_at) )
-              except:
-                  print( ">> %s old tweet errored" % ( favorite.id ) )
+                try:
+                    if favorite.created_at < datetime.now() - timedelta(days=max_age):
+                        print(
+                            ">> %s unliking old tweet from %s"
+                            % (favorite.id, favorite.created_at)
+                        )
+                        self.unlike(favorite.id)
+                    else:
+                        print(
+                            ">> %s keeping like on old tweet from %s"
+                            % (favorite.id, favorite.created_at)
+                        )
+                except:
+                    print(">> %s old tweet errored" % (favorite.id))
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Unlike or delete (re-)tweets (and optionally export them first). Set other parameters via configuration file (default: "settings.ini" in script directory) or arguments. Set arguments will overrule the configuration file.')
-    parser.add_argument("--block", default=None, dest="target_user", help='Target user to block', type=str, action="store")
-    parser.add_argument("--blockchain", default=None, dest="target_user_chain", help='Target user to block followers', type=str, action="store")
-    parser.add_argument("--unretweet", default=None, dest="untweet_id", help='Target id to delete if not retweet', type=str, action="store")
-    parser.add_argument("--unlike", default=None, dest="unlike_id", help='Target id to delete if not retweet', type=str, action="store")
-    parser.add_argument("--oldlikes", default=None, dest="old_likes_age", help='Tweets older then X days', type=int, action="store")
+    parser = argparse.ArgumentParser(
+        description='Unlike or delete (re-)tweets (and optionally export them first). Set other parameters via configuration file (default: "settings.ini" in script directory) or arguments. Set arguments will overrule the configuration file.'
+    )
+    parser.add_argument(
+        "--block",
+        default=None,
+        dest="target_user",
+        help="Target user to block",
+        type=str,
+        action="store",
+    )
+    parser.add_argument(
+        "--blockchain",
+        default=None,
+        dest="target_user_chain",
+        help="Target user to block followers",
+        type=str,
+        action="store",
+    )
+    parser.add_argument(
+        "--unretweet",
+        default=None,
+        dest="untweet_id",
+        help="Target id to delete if not retweet",
+        type=str,
+        action="store",
+    )
+    parser.add_argument(
+        "--unlike",
+        default=None,
+        dest="unlike_id",
+        help="Target id to delete if not retweet",
+        type=str,
+        action="store",
+    )
+    parser.add_argument(
+        "--oldlikes",
+        default=None,
+        dest="old_likes_age",
+        help="Tweets older then X days",
+        type=int,
+        action="store",
+    )
     args = parser.parse_args()
     twitter = TwitterClean(args)
     if args.target_user:
-        twitter.block_user(args.target_user,args.target_user)
+        twitter.block_user(args.target_user, args.target_user)
     if args.target_user_chain:
-        twitter.block_user(args.target_user_chain,args.target_user_chain)
+        twitter.block_user(args.target_user_chain, args.target_user_chain)
         twitter.block_followers(args.target_user_chain)
     if args.untweet_id:
         twitter.unretweet(args.untweet_id)
